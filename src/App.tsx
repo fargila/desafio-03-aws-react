a1e7a839dc60e789616773ead4f44b90d819d7cf
fargila <atilahunesco@gmail.com>
2024-11-05 17:17:33 -0300

    feat: adicionando 3/4 dos componentes sem lógica, mas estilizados.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import CreateCard from "./components/CreateCardExp"
 import LoginPage from "./pages/LoginPage"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <LoginPage />
+      <CreateCard />
     </>
   )
 }
